5532bf397ddd96ff6b32607dd0cfde8a14bd8566
Kaita Nakamura <kaita.nakamura0830@gmail.com>
2025-06-30 07:55:50 +0900

    クライアントコードをクラスベースにリファクタリング
    - index.js を削除し、TypeScript完全移行
    - UnixSocketClient クラスを作成してコードを整理
    - エラーハンドリング改善とログメッセージの日本語化
    - package.json の改善（dev スクリプト追加）
    
    Signed-off-by: Kaita Nakamura <kaita.nakamura0830@gmail.com>

--- client/dist/index.js
+++ client/dist/index.js
@@ -1,16 +1,32 @@
-import net from 'net';
-const client = net.createConnection('/tmp/unix.sock', () => {
-    console.log('connected to server');
-    client.write('client.data');
-});
-client.on('data', (data) => {
-    console.log(data.toString());
-    client.end();
-});
-client.on('end', () => {
-    console.log('disconnected from server');
-});
-client.on('error', (err) => {
-    console.error(err.message);
-});
+import { Socket } from 'net';
+class UnixSocketClient {
+    constructor(socketPath = '/tmp/unix.sock') {
+        this.socketPath = socketPath;
+        this.socket = new Socket();
+        this.setupEventListeners();
+    }
+    connect() {
+        this.socket.connect(this.socketPath, () => {
+            console.log('接続完了: サーバーに接続しました');
+            this.socket.write('client.data');
+        });
+    }
+    disconnect() {
+        this.socket.end();
+    }
+    setupEventListeners() {
+        this.socket.on('data', (data) => {
+            console.log(`データ受信: ${data.toString()}`);
+            this.disconnect();
+        });
+        this.socket.on('end', () => {
+            console.log('切断: サーバーから切断されました');
+        });
+        this.socket.on('error', (err) => {
+            console.error(`エラー: ${err.message}`);
+        });
+    }
+}
+const client = new UnixSocketClient();
+client.connect();
 //# sourceMappingURL=index.js.map
